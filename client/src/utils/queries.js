b1f264f472ae896b1421608c9b45fa42e6f1e02c
The Duz <tamara.dusenbury@gmail.com>
2023-06-04 15:57:25 -0700

    update initial file structure

---
+++ client/src/utils/queries.js
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+      }
+    }
+  }
+`;
+
+
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        thoughtAuthor
+        createdAt
+      }
+    }
+  }
+`;
