1019fda92a607975a50fc3033c60043ee6fb3d09
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-06-08 13:02:49 -0700

    add styling

--- client/src/pages/EventsAdd.js
+++ client/src/pages/EventsAdd.js
@@ -82,37 +82,37 @@ const СreateEvent = () => {
         <div className="event-page-container">
             <h1>Create Event</h1>
             <form className="event-form">
-                <div className="form-row">
-                    <div className="form-column">
-                        <label>Name:</label>
-                        <input type="text" name="name" value={eventData.name} onChange={handleInputChange} />
-                    </div>
+                <div class="input-area">
+                    <label>Name:</label>
+                    <input type="text" name="name" value={eventData.name} onChange={handleInputChange} />
                 </div>
-                <div className="form-row">
-                    <div className="form-column">
-                        <label>Location:</label>
-                        <textarea type="text" name="location" value={eventData.location} onChange={handleInputChange} />
-                    </div>
+                <div class="input-area">
+                    <label>Location:</label>
+                    <textarea type="text" name="location" value={eventData.location} onChange={handleInputChange} />
                 </div>
                 <div id="dateAndTime" className="form-row">
                     <div className="form-column">
-                        <label>Start Time:</label>
-                        <MyClock selectedTime={eventData.startTime} onTimeChange={(time) => setEventData((prevData) => ({ ...prevData, startTime: time }))} />
+                        <div className="Time">
+                            <label>Start Time:</label>
+                            <MyClock selectedTime={eventData.startTime} onTimeChange={(time) => setEventData((prevData) => ({ ...prevData, startTime: time }))} />
+                        </div>
+                        <div className="Date">
+                            <label>Start Date:</label>
+                            <MyCalendar selectedDate={eventData.startDate} onDateChange={(date) => setEventData((prevData) => ({ ...prevData, startDate: date }))} />
+                        </div>
                     </div>
                     <div className="form-column">
-                        <label>Start Date:</label>
-                        <MyCalendar selectedDate={eventData.startDate} onDateChange={(date) => setEventData((prevData) => ({ ...prevData, startDate: date }))} />
-                    </div>
-                    <div className="form-column">
-                        <label>End Time:</label>
-                        <MyClock selectedTime={eventData.endTime} onTimeChange={(time) => setEventData((prevData) => ({ ...prevData, endTime: time }))} />
-                    </div>
-                    <div className="form-column">
-                        <label>End Date:</label>
-                        <MyCalendar selectedDate={eventData.endDate} onDateChange={(date) => setEventData((prevData) => ({ ...prevData, endDate: date }))} />
+                        <div className="Time">
+                            <label>End Time:</label>
+                            <MyClock selectedTime={eventData.endTime} onTimeChange={(time) => setEventData((prevData) => ({ ...prevData, endTime: time }))} />
+                        </div>
+                        <div className="Date">
+                            <label>End Date:</label>
+                            <MyCalendar selectedDate={eventData.endDate} onDateChange={(date) => setEventData((prevData) => ({ ...prevData, endDate: date }))} />
+                        </div>
                     </div>
                 </div>
-                <div>
+                <div class="input-area">
                     <label>Description:</label>
                     <textarea name="description" value={eventData.description} onChange={handleInputChange} />
                 </div>
