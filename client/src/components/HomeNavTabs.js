87dea597f411560077151ec9f17a7616e5aaae46
Bahareh <mer_ir@yahoo.com>
2023-06-09 20:11:32 -0700

    change the styles of homeNavTab, clubNamTab and home page

--- client/src/components/HomeNavTabs.js
+++ client/src/components/HomeNavTabs.js
@@ -1,58 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "../assets/styles/HomeNavTabs.css";
+import '../assets/styles/HomeNavTabs.css';
 
-import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarToggler,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBCollapse,
-  MDBIcon
-} from 'mdb-react-ui-kit';
-
-
-export default function HomeNavTabs() {
-  const [showNav, setShowNav] = useState(false);
-
-  return (
-    <div className="nav-bar">
-    <>
-    <MDBNavbar className='navright' expand='lg' light bgColor='light'>
-      <MDBContainer>
-        <MDBNavbarBrand href='#' className="p-3"></MDBNavbarBrand>
-        <MDBNavbarToggler
-          type='button'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowNav(!showNav)}
-        >
-          <MDBIcon icon='bars' fas />
-        </MDBNavbarToggler>
-        <MDBCollapse navbar show={showNav}>
-          <MDBNavbarNav>
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='/'>
-                Home
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='/about'>About</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='/login'>Login/Signup</MDBNavbarLink>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
-    </>
-    </div>
-  );
+function HomeNavTabs() {
+    return (
+        <ul className="nav justify-content-start">
+            <li className="nav-item">
+              <Link
+                className="btn btn-block btn-primary"
+                to="/">
+                Home  
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="btn btn-block btn-primary"
+                to="/about">
+                About  
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="btn btn-block btn-primary custom-link-style"
+                style={{ whiteSpace: 'normal' }}
+                to="/login">
+                Login / Signup 
+              </Link>  
+            </li>
+        </ul>
+    );
 }
+
+export default HomeNavTabs;
 
 
 
