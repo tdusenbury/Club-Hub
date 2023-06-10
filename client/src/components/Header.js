22c2c8417e99432630a2ad5fec761866d9c84790
Bahareh <mer_ir@yahoo.com>
2023-06-10 16:12:55 -0700

    add anime

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import anime from 'animejs';
 import Auth from '../utils/auth';
 import HomeNavTabs from './HomeNavTabs';
 import ClubNavTabs from './ClubNavTabs';
@@ -8,6 +7,19 @@ import "../assets/styles/HomeNavTabs.css"
 import ClubHubLogo from '../assets/images/Clubhub-logo.png';
 
 const Header = () => {
+  const clubHubTitle = document.querySelector('.club-hub-title');
+  anime({
+    targets: clubHubTitle,
+    keyframes: [
+      {translateY: -20},
+      {translateX: 75},
+      {translateY: 30},
+      {translateX: 0},
+      {translateY: 0}
+    ],
+    duration: 4000,
+    easing: 'easeOutElastic(1, .8)',
+  });
   return (
     <header className="bg-primary header d-flex flex-column flex-wrap justify-content-center">
       <div className="d-flex flex-row">
@@ -17,7 +29,7 @@ const Header = () => {
         className='logo-shift'
         />
       </div>
-      <h1 className='d-flex justify-content-center text-light size'>Club Hub</h1>
+      <h1 className='club-hub-title d-flex justify-content-center text-light size'>Club Hub</h1>
       {Auth.loggedIn() ? (
               <ClubNavTabs/>
           ) : (
