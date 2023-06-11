5e63d60ed1b9ea5333bc6d18289af6e23c707d13
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-06-10 22:13:29 -0700

    fix event manager

--- client/src/pages/EventsManager.js
+++ client/src/pages/EventsManager.js
@@ -15,25 +15,10 @@ export default function EventsManager() {
     }
 
     return (
-        <div style={{width:"100&"}}>
-             
-             <div>
-                 <h2
-          className="text-center title-size"
-          style={{
-            fontSize: "60px",
-            fontWeight: "bold",
-            textDecoration: "underline",
-            color: "white",
-            padding: "20px",
-            backgroundColor: "#28C8A8",
-           
-          }}
-        >
-          Event Manager
-        </h2>
-        <br />
-        </div>
+        <div className=" page-container">
+            <h2 className="text-center title-size" id="title" >
+                Event Manager
+            </h2>
             <div className="card-container">
                 {events.length
                     ? events.map((event) => (<EventCard key={event._id} event={event} />))
@@ -44,6 +29,6 @@ export default function EventsManager() {
                 <button className="add-event-button">Add an Event</button>
             </Link>
             {loading && <div className="loader"></div>}
-        </div>
+        </div >
     );
 }
